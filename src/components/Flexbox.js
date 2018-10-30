695b57aea94d9fd2d857f8e3886a2eb87c08ffe4
Haofu Zhu <haofuzhu@Alyssas-MacBook-Pro-2.local>
2018-10-30 17:04:30 +1100

    Add flex box practice

---
+++ src/components/Flexbox.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Flexbox extends Component {
+  render() {
+    return (
+      <div className="box-wrapper">
+        <div className="box-1"></div>
+        <div className="box-2"></div>
+      </div>
+    );
+  }
+}
+
+export default Flexbox;
